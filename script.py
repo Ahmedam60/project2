7e850331a8406c3bd63f84c2780db9c3092c8d53
Ahmedam60 <ahmedamin.khalil@gmail.com>
2021-08-24 19:19:43 +0200

    modify script to be readable

--- script.py
+++ script.py
@@ -1,36 +1,48 @@
-import csv
+## CREATED AT 24-AUG-2021 ##
+#AUTHOR:AHMED AMIN#
+
+#USED LIBRARY (BEUTIFULSOUP,REQUESTS AND CSV)
+
 
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.gov.uk/search/news-and-communications"
+import csv
 
-page = requests.get(url)
+#FUNCTION TO CONVERT ELEMENTS TO STRING ELEMENTS
+def convert_string(elements):
+    item = []
+    for i in elements:
+        item.append(i.string)
 
-soup = BeautifulSoup(page.content,'html.parser')
-print(soup.title.string)
-bs_titles = soup.findAll("a",class_="gem-c-document-list__item-title")
-titles = []
-for title in bs_titles:
-    titles.append(title.string)
+    return item
 
-#print(titles)
+#FUNCTION TO CREATE AND STORE DATA INTO FILE
+def load_data(file,header,titles,description):
+    with open(r"D:\ALGO\Finance\data2.csv", "w", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(header)
 
-bs_desc = soup.findAll('p',class_="gem-c-document-list__item-description")
-description = []
-for desc in bs_desc:
-    description.append(desc.string)
+        for i in range(len(titles)):
+            row = [titles[i], description[i]]
+            writer.writerows([row])
 
-#print(description)
-dic = dict(zip(titles,description))
 
-#print(dic)
-header = ["Titles","Description"]
+def main():
 
-with open(r"D:\ALGO\Finance\data1.csv","w",encoding="utf-8") as csvfile:
-    writer = csv.writer(csvfile, delimiter =",")
-    writer.writerow(header)
+    #URL TO SCRAPE
+    url = "https://www.gov.uk/search/news-and-communications"
+    page = requests.get(url)
 
-    for i in range(len(titles)):
-        row = [titles[i], description[i]]
-        #writer.writerow(row)
-        writer.writerows([row])
+    #CREATE BEUTIFULSOUP OBJECT
+    soup = BeautifulSoup(page.content,'html.parser')
+
+    #GET ALL TITLES WITH SPECIFIC CLASS
+    titles = soup.findAll('a', class_='gem-c-document-list__item-title')
+#    #GET ALL DESCRIPTION WITH SPECIFIC CLASS
+    description = soup.findAll('p', class_='gem-c-document-list__item-description')
+    header = ['Titles' ,'Description']
+    titles = convert_string(titles)
+    description = convert_string(description)
+    load_data(r"D:\ALGO\Finance\data2.csv", header , titles , description)
+
+main()
